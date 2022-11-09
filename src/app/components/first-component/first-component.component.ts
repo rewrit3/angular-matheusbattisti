f78f5cc4cfebb1c1d8d98afac662c5a96aad3544
Adolfo Cruz <adolfo@rewrite.com.br>
2022-11-09 18:08:39 -0300

    main - aula #05

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.scss']
 })
 export class FirstComponentComponent implements OnInit {
+  name: String = 'Adolfo';
+  age: number = 33;
+  job = 'Web Developer';
+  hobbies = ['Futebol', 'Música', 'E-Sports'];
+  car = {
+    name: 'Corsa',
+    year: 2009,
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
